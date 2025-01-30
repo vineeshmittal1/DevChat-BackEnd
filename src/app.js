c6a90aa630c7b13b000f9f9b8f942feaed0547fd
vineeshmittal1 <vineeshmittal19@gmail.com>
2025-01-30 14:04:09 +0530

    Testing various HTTP methods

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
 const app = express();
-app.use("/test", (req, res) => {
-  res.send("Hello from the serve!");
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Vineesh", lastName: "Mittal" });
 });
-app.listen(3000, () => {
-  console.log("Server is successfully listening on port 3000... ");
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the database");
+});
+app.delete("/user", (req, res) => {
+  res.send("Deleted Successfully");
+});
+app.use("/test", (req, res) => {
+  res.send("Hello from the server!");
+});
+app.listen(7777, () => {
+  console.log("Server is successfully listening on port 300... ");
 });
