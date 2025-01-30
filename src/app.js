5210b4430cf58d11783433961c91ebc27ae95aa1
vineeshmittal1 <vineeshmittal19@gmail.com>
2025-01-31 00:47:11 +0530

    Wildcard-Error Handling

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
 
-app.use("/admin", adminAuth);
-
-app.get("/user", userAuth, (req, res) => {
-  res.send("User Data Sent");
+app.get("/getUserData", (req, res) => {
+  try {
+    throw new Error("hello");
+    res.send("User Data Sent");
+  } catch (err) {
+    res.status(500).send("Some Error contact the support team");
+  }
 });
 
+app.use("/", (err, req, res, next) => {
+  if (err) res.status(500).send("Something went wrong");
+});
+
+
 app.listen(7777, () => {
-  console.log("Server is successfully listening on port 300... ");
+  console.log("Server is successfully listening on port 7777... ");
 });
