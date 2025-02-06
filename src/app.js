392fae6b7ef34d8c58742171b7f7aaef9ddba0b1
vineeshmittal1 <vineeshmittal19@gmail.com>
2025-02-06 22:33:42 +0530

    Added Cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -36,7 +36,7 @@ authRouter.post("/login", async (req, res) => {
       res.cookie("token", token, {
         expires: new Date(Date.now() + 8 * 3600000),
       });
-      res.send("Login Successful");
+      res.send(user);
     } else throw new Error("Invalid credentials");
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
