8e7be15c51dc451abac321f0108aeaf5f36e15c9
vineeshmittal1 <vineeshmittal19@gmail.com>
2025-02-12 00:42:05 +0530

    Added Token In SignUp

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -17,8 +17,13 @@ authRouter.post("/signup", async (req, res) => {
       password: passwordHash,
     });
 
-    await user.save();
-    res.send("User Added Successfully");
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+    res.json({ message: "User Added Successfully", data: savedUser });
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
   }
